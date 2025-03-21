59068b74cd898776220cb61f9d524b1fd05dd37b
Braily Guzman <brailyg@outlook.com>
2025-03-21 11:44:13 -0400

    Add browser router and route

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from 'react-router';
+import Home from './components/Home/Home.jsx';
+
 const App = () => {
-  return <div id="app"></div>;
+  return (
+    <div id="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ app/src/components/Recipes/Recipes.jsx
@@ -0,0 +1,11 @@
+const Recipes = () => {
+  /*
+  This component will be passed in a search query
+  On render, it will:
+    1. Fetch recipes based on the query
+    2. Render the recipes, or an error if something went wrong.
+  */
+  return <div id="recipes"></div>;
+};
+
+export default Recipes;
