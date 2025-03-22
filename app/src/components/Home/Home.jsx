455c464524d250e7c9484c0e345c39c130e95b93
Braily Guzman <brailyg@outlook.com>
2025-03-22 10:47:24 -0400

    Fetch random recipe on render, and update comment on the Recipe component

--- app/src/components/Home/Home.jsx
+++ app/src/components/Home/Home.jsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from 'react';
+import Search from '../Shared/Search.jsx';
+import handleFetch from '../../handlers/handleFetch.js';
+import { randomEP } from '../../constants.js';
+
 const Home = () => {
-  return <div>This is the home page</div>;
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const fetchRandomRecipe = async () => {
+      const [data, error] = await handleFetch(randomEP);
+
+      if (error) {
+        return null;
+      }
+
+      return data;
+    };
+
+    console.log(fetchRandomRecipe());
+  }, []);
+
+  return (
+    <div>
+      <Search query={query} setQuery={setQuery} />
+    </div>
+  );
 };
 
 export default Home;
