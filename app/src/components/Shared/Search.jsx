f0856141acf80c77dd1effa15bd65adecb2384b9
Braily Guzman <brailyg@outlook.com>
2025-03-22 10:46:13 -0400

    Implement Search shared component

---
+++ app/src/components/Shared/Search.jsx
@@ -0,0 +1,13 @@
+const Search = ({ query, setQuery }) => {
+  return (
+    <input
+      className="search-bar"
+      value={query}
+      placeholder="Search for a recipe"
+      onChange={(e) => setQuery(e.target.value)}
+      aria-label="Search Bar"
+    />
+  );
+};
+
+export default Search;
